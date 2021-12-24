45db78fe5dc771e905fc3964d882058ec10ccddc
Sulekha02112001 <kumarisulekha02112001@gmail.com>
2021-12-24 22:01:23 +0530

    test

--- httpApp/main.py
+++ httpApp/main.py
@@ -1,5 +1,4 @@
 import boto3
-from flask import jsonify
 from flask import Flask, request
 from flask.templating import render_template
 app = Flask(__name__)
@@ -8,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     s3_client = boto3.client('s3', aws_access_key_id='AKIATJZDC6CLFZY4R56Q',
-                             aws_secret_access_key='/ 5xxf+KQxngou8wXx/RJIOPCPipb+8mif+RBh5/R',)
+                             aws_secret_access_key='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',)
     response = s3_client.get_object(
         Bucket='helmrepo', Key='charts/index.yaml')
     data = response['Body'].read()
